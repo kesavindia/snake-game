cc2fab127b047b991ba2495bd83fc60137022234
kesavindia <kesavindia83@gmail.com>
2023-05-31 23:13:51 +0530

    second commit

--- snake.js
+++ snake.js
@@ -9,6 +9,7 @@ let foodY;
 let xVel=20;
 let yVel=0;
 let Score=0;
+scoreText.textContent=Score;
 let active = true;
 let started=false;
 let paused=false;
@@ -23,4 +24,119 @@ function startGame(){
     displayFood();
     drawSnake();    
 }
+function clearBoard(){
+    context.fillStyle="#212121"
+    //fillRect(xStart,yStart,width,height)
+    fillRect(0,0,Width,Height)
+}
+function createFood(){
+    foodX = Math.floor(Math.random()*Width/UNIT)*UNIT;
+    foodY = Math.floor(Math.random()*Height/UNIT)*UNIT;
+}
+function displayFood(){
+    context.fillStyle='red';
+    context.fillRect(foodX,foodY,UNIT,UNIT);
+}
+function drawSnake(){
+    context.fillStyle="aqua";
+    context.strokeStyle = '#212121';
+    snake.forEach((snakepart)=>{
+        context.fillRect(snakepart.x,snakepart.y,UNIT,UNIT);
+        context.strokeRect(snakepart.x,snakepart.y,UNIT,UNIT);
+    })
+}
+const moveSnake=()=>{
+    const head={x:snake[0].x+xVel,y:snake[0].y+yVel}
+    snake.unshift(head);
+    if(snake[0].x==foodX && snake[0].y==foodY){
+        Score+=1;
+        createFood();
+    }
+    else{
+        snake.pop();
+    }
+}
+function nextTick(){
+    if(active && !paused){
+        setTimeout(() => {
+            clearBoard();
+            displayFood();
+            moveSnake();
+            drawSnake();
+            checkGameOver();
+            nextTick();
+        }, 200);
+    }
+    else if(!active){
+        clearBoard();
+        context.font = "bold 50px serif";
+        context.fillStyle = "white";
+        context.textAlign = "center";
+        context.fillText("Game Over!!",WIDTH/2,HEIGHT/2)
+    }
+}
 
+function keyPress(event){
+    if(!started){
+        started = true;
+        nextTick();
+    }
+    //pause when space is pressed
+    if(event.keyCode===32){
+        console.log('clicked')
+        if(paused){
+            paused = false;
+            nextTick();
+        }
+        else{
+            paused = true;
+        }
+    }
+    const LEFT = 37
+    const UP = 38
+    const RIGHT = 39
+    const DOWN = 40
+
+    switch(true){
+        //left key pressed and not going right
+        case(event.keyCode==LEFT  && xVel!=UNIT):
+            xVel=-UNIT;
+            yVel = 0;
+            break;
+        //right key pressed and not going left
+        case(event.keyCode==RIGHT && xVel!=-UNIT):
+            xVel=UNIT;
+            yVel=0;
+            break;
+        //Up key pressed and not going down
+        case(event.keyCode==UP && yVel!=UNIT):
+            xVel=0;
+            yVel=-UNIT;
+            break;
+        //down key pressed and not going up
+        case(event.keyCode==DOWN && yVel!=-UNIT):
+            xVel=0;
+            yVel=UNIT;
+            break;
+
+    }
+}
+
+function checkGameOver(){
+    switch(true){
+        case(snake[0].x<0):
+        case(snake[0].x>=WIDTH):
+        case(snake[0].y<0):
+        case(snake[0].y>=HEIGHT):
+            active=false;
+            break;
+    }
+	//check snake head collision with snake body
+	   for(let i = 1; i < snake.length; i+=1){
+        if(snake[i].x == snake[0].x && snake[i].y == snake[0].y){
+            active = false;
+        }
+    }
+}
+
+    
